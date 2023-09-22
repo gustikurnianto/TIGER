4be5508cc9a6ae607fbb4d2722718c5cfc7cf72c
gustikurnianto <117699434+gustikurnianto@users.noreply.github.com>
2023-09-23 02:08:20 +0700

    Create cdbpetcarejs10.js

---
+++ cdbpetcarejs10.js
@@ -0,0 +1,31 @@
+var modalreservasi = document.getElementById('modalreservasi');
+    var newanddone = document.getElementById('newanddone');
+
+    var modalClosereservasi = document.getElementById('closereservasi');
+    modalClosereservasi.addEventListener('click', function() { 
+        modalreservasi.style.display = "none";
+        $('#newanddone').show();
+        $('#menutools').show();
+    });
+
+    document.addEventListener('click', function (e) { 
+        if (e.target.className.indexOf('imgRcdclass') !== -1) {
+            var imgreservasi = e.target;
+            var modalImgreservasi = document.getElementById("contentreservasi");
+            modalreservasi.style.display = "block";
+            modalImgreservasi.src = imgreservasi.src;
+            $('#newanddone').hide();
+            $('#menutools').hide();
+        }
+    });
+
+    document.addEventListener('click', function (e) { 
+        if (e.target.className.indexOf('imgRcdclassDone') !== -1) {
+            var imgreservasi = e.target;
+            var modalImgreservasi = document.getElementById("contentreservasi");
+            modalreservasi.style.display = "block";
+            modalImgreservasi.src = imgreservasi.src;
+            $('#newanddone').hide();
+            $('#menutools').hide();
+        }
+    });
