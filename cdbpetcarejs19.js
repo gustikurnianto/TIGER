38c311dcaefa82d0faed0d09d4672f0aed13561e
gustikurnianto <117699434+gustikurnianto@users.noreply.github.com>
2023-09-23 02:19:19 +0700

    Create cdbpetcarejs19.js

---
+++ cdbpetcarejs19.js
@@ -0,0 +1,11 @@
+const mouseWheelz = document.querySelector('.scrolldrg');
+
+        mouseWheelz.addEventListener('wheel', function(e) {
+            const racez = 50;
+
+            if (e.deltaY > 0)
+                mouseWheelz.scrollLeft += racez;
+            else
+                mouseWheelz.scrollLeft -= racez;
+                e.preventDefault();
+        });
