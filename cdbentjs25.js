b53b4b19732f3711d2488233ead9b5efc931882d
gustikurnianto <117699434+gustikurnianto@users.noreply.github.com>
2023-09-22 23:35:45 +0700

    Create cdbentjs25.js

---
+++ cdbentjs25.js
@@ -0,0 +1,35 @@
+        var offlineMsg = document.getElementById("offline-message");
+
+        function _showOfflineMessage(){
+            $('#klikduakali').remove();
+            $('#loadingselesailogin').remove();
+            window.getUsername = function() {
+                return false;
+            }
+            window.onload = function() {
+                return false;
+            }
+            document.getElementsByTagName("html")[0].setAttribute("offline",true);
+            offlineMsg.setAttribute("aria-hidden","false");
+            setTimeout(function(){
+                $('#klikduakali').remove();
+                $('#loadingselesailogin').remove();
+                window.getUsername = function() {
+                    return false;
+                }
+                window.onload = function() {
+                    return false;
+                }
+                offlineMsg.focus();
+            },400);
+        }
+
+        function _hideOfflineMessage(){
+            document.getElementsByTagName("html")[0].setAttribute("offline",false);
+            offlineMsg.setAttribute("aria-hidden","true");
+            $('#online-message').show().delay(3000).fadeOut(300);
+        }
+
+        window.addEventListener("offline", _showOfflineMessage, false);
+
+        window.addEventListener("online", _hideOfflineMessage, false);
