33c8f2eaa97cc4465fb47f11cd9d3f562e170c1f
gustikurnianto <117699434+gustikurnianto@users.noreply.github.com>
2023-09-23 02:17:26 +0700

    Create cdbpetcarejs17.js

---
+++ cdbpetcarejs17.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById('modal');
+
+    var modalClose = document.getElementById('close');
+        modalClose.addEventListener('click', function() { 
+        modal.style.display = "none";
+        $('#newanddone').hide();
+    });
+
+    document.addEventListener('click', function (e) { 
+        if (e.target.className.indexOf('borderlistz') !== -1) {
+            var img = e.target;
+            var modalImg = document.getElementById("content");
+            modal.style.display = "block";
+            modalImg.src = img.src;
+            $('#newanddone').hide();
+        }
+    });
